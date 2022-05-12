9f4fe1f734316b35158ffc3d2e5361e0dd0197b3
Zinatoy <98818055+Zinatoy@users.noreply.github.com>
2022-05-12 17:09:29 +0600

    apps,settings,url,static

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from apps.settings.models import Settings
 
 # Create your views here.
+
+def index(request):
+    home =  Settings.objects.latest('id')
+    context = {
+        'home' : home
+    }
+
+    return render(request , 'index.html', context)
+
--- booking/settings.py
+++ booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=)lf^(2+g@@63#8x3y41w3!x)y-st23kcc-a^*0i8j)b-q40l3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #apps
+    'apps.settings',
+    'apps.hotels',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '/static/'
+STATICFILES_DIRS= [BASE_DIR / 'static']
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT= BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
